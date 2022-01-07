420c517b9503c00bb79a76e0893b7d1f376a7a1e
Art <d.art.shishkin@gmail.com>
2022-01-07 19:50:42 +0200

    29. Hands on Demo: Passing Parameters via the Event Object (#2 Section 2: Serverless Foundation - AWS Lambda)

---
+++ greetMe/index.js
@@ -0,0 +1,15 @@
+const greeting = {
+    "en": "Hello",
+    "fr": "Bonjour",
+    "hi": "Namaste",
+    "es": "Hola",
+    "pt": "Olá",
+    "ur": "Assalamo aleikum",
+    "it": "Ciao",
+    "de": "Hallo"
+}
+
+exports.handler = async (event) => {
+
+
+}
