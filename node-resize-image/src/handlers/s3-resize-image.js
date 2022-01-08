afbb7e39cdb121f66934473e903c6a7bde43cec6
Art <d.art.shishkin@gmail.com>
2022-01-08 14:07:11 +0200

    33.2 Serverless Image Resizing - GetObject (#2)

--- node-resize-image/src/handlers/s3-resize-image.js
+++ node-resize-image/src/handlers/s3-resize-image.js
@@ -1,26 +1,35 @@
+const im = require('imagemagick');
+const fs = require('fs');
+const os = require('os');
+// const uuidv4 = require('uuid/v4');
+const {promisify} = require('util');
+
 const AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-north-1'});
 
 const s3 = new AWS.S3();
 
-/**
- * A Lambda function that logs the payload received from S3.
- */
 exports.handler = async (event, context) => {
-  // All log statements are written to CloudWatch by default. For more information, see
-  // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-  const getObjectRequests = event.Records.map(record => {
-    const params = {
-      Bucket: record.s3.bucket.name,
-      Key: record.s3.object.key
-    };
-    return s3.getObject(params).promise().then(data => {
-      console.info(data.Body.toString());
-    }).catch(err => {
-      console.error("Error calling S3 getObject:", err);
-      return Promise.reject(err);
-    })
-  });
-  return Promise.all(getObjectRequests).then(() => {
-    console.debug('Complete!');
-  });
+
+    const filesProcessed = event.Records.map(async (record) => {
+        const params = {
+            Bucket: record.s3.bucket.name,
+            Key: record.s3.object.key
+        };
+        try {
+            const data = await s3.getObject(params).promise();
+            const modifiedData = {
+                ...data,
+                "Body": []
+            };
+            console.info(JSON.stringify(modifiedData));
+        } catch (err) {
+            console.error("Error calling S3 getObject:", err);
+            return Promise.reject(err);
+        }
+    });
+
+    await Promise.all(filesProcessed);
+    console.debug('Done!');
+    return "Done!";
 };
