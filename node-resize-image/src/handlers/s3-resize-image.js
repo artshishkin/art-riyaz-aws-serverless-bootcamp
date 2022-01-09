c61f8cf5af24060163640a9160f24720fdf74376
Art <d.art.shishkin@gmail.com>
2022-01-09 05:28:44 +0200

    34.1 Hands on Demo: Handling S3 Events in Lambda (#2)

--- node-resize-image/src/handlers/s3-resize-image.js
+++ node-resize-image/src/handlers/s3-resize-image.js
@@ -1,8 +1,12 @@
 const im = require('imagemagick');
 const fs = require('fs');
 const os = require('os');
-// const uuidv4 = require('uuid/v4');
+const uuid = require('uuid');
 const {promisify} = require('util');
+
+const resizeAsync = promisify(im.resize);
+const readFileAsync = promisify(fs.readFile);
+const unlinkAsync = promisify(fs.unlink);
 
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-north-1'});
@@ -12,17 +16,47 @@ const s3 = new AWS.S3();
 exports.handler = async (event, context) => {
 
     const filesProcessed = event.Records.map(async (record) => {
-        const params = {
-            Bucket: record.s3.bucket.name,
-            Key: record.s3.object.key
-        };
+        const bucket = record.s3.bucket.name;
+        const filename = record.s3.object.key;
         try {
-            const data = await s3.getObject(params).promise();
-            const modifiedData = {
-                ...data,
-                "Body": []
+
+            // Get the file from S3
+            const params = {
+                Bucket: bucket,
+                Key: filename
             };
-            console.info(JSON.stringify(modifiedData));
+
+            const data = await s3.getObject(params).promise();
+            console.log(JSON.stringify({...data, Body: []}));
+
+            // Resize the file
+            let tempFile = os.tmpdir() + '/' + uuid.v4() + '.png';
+            let resizeArgs = {
+                srcData: data.Body,
+                dstPath: tempFile,
+                width: 128
+            };
+            await resizeAsync(resizeArgs);
+
+            // Read the resized file
+            const resizedData = await readFileAsync(tempFile);
+
+            // Upload the new file to S3
+            const targetFilename = filename.replace("original/", "thumbnail/");
+
+            const putParams = {
+                Bucket: bucket,
+                Key: targetFilename,
+                Body: new Buffer(resizedData),
+                ContentType: 'image/png'
+            };
+
+            const savedResizedImageData = await s3.putObject(putParams).promise();
+
+            console.debug(JSON.stringify(savedResizedImageData));
+
+            return await unlinkAsync(tempFile);
+
         } catch (err) {
             console.error("Error calling S3 getObject:", err);
             return Promise.reject(err);
