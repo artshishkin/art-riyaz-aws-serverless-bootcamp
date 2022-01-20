c27901a3adfed73580575c84d47155f71f35c80f
Art <d.art.shishkin@gmail.com>
2022-01-20 15:29:28 +0200

    105.2 Dynamic Lambda Handlers - Calculate Square Function (#8)

--- Section5-DeepDive/dynamic-lambda-handlers/src/calculateSquare.js
+++ Section5-DeepDive/dynamic-lambda-handlers/src/calculateSquare.js
@@ -1,33 +1,27 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'eu-north-1' });
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+var lambda = new AWS.Lambda();
+
 exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
+    let number = event.number;
+    let payload = JSON.stringify({
+        operation: "multiply",
+        input: {
+            operand1: number,
+            operand2: number
         }
-    } catch (err) {
-        console.log(err);
-        return err;
+    });
+
+    let params = {
+        FunctionName: process.env.CALCULATOR_FUNCTION_NAME,
+        InvocationType: "RequestResponse",
+        Payload: payload
     }
 
-    return response
+    let data = await lambda.invoke(params).promise();
+
+    let result = JSON.parse(data.Payload);
+
+    return result.body;
 };
