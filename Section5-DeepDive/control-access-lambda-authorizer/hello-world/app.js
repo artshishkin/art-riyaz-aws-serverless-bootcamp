959960c6cb4eb3169cc77ca4e42b32cff74a2ac2
Art <d.art.shishkin@gmail.com>
2022-01-21 14:20:28 +0200

    114. Using Lambda Authorizer Context Object for Body Mapping in API Gateway (or through HttpProxyRequest object) (#8)

--- Section5-DeepDive/control-access-lambda-authorizer/hello-world/app.js
+++ Section5-DeepDive/control-access-lambda-authorizer/hello-world/app.js
@@ -1,5 +1,3 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
 let response;
 
 /**
@@ -7,21 +5,23 @@ let response;
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
+
+    // console.log(JSON.stringify(event));
+    console.log(JSON.stringify(context));
+
     try {
-        // const ret = await axios(url);
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
+                message: 'hello ' + event.requestContext.authorizer.name
             })
         }
     } catch (err) {
