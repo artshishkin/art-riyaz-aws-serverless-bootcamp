89c929a3f10a5e5089ee8c5f710d6ac4d8b28202
Art <d.art.shishkin@gmail.com>
2022-01-27 10:19:01 +0200

    153.2 REST API - Retrieve User (GET) (#12)

---
+++ Section7-SAM/rest-api/functions/get.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+AWS.config.update({region: process.env.AWS_REGION});
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const tableName = process.env.TABLE_NAME;
+
+exports.handler = async (event) => {
+
+    let userId = event.pathParameters.userId;
+
+    let data = await dynamodb.get({
+        TableName: tableName,
+        Key: {
+            userId: userId
+        }
+    }).promise();
+
+    if (data.Item) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Item)
+        };
+    } else {
+        throw new Error("User not found");
+    }
+}
