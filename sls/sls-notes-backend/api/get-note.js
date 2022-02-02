f245841699019f65670ad6ae0965572b4c966d83
Art <d.art.shishkin@gmail.com>
2022-02-02 11:57:20 +0200

    204.1 Writing the Lambda Handlers - get single note (#17)

--- sls/sls-notes-backend/api/get-note.js
+++ sls/sls-notes-backend/api/get-note.js
@@ -1,7 +1,5 @@
 'use strict';
 
-'use strict';
-
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-north-1'});
 
@@ -9,16 +7,37 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.NOTES_TABLE;
 
 const util = require('./util');
+const _ = require('underscore');
 
 exports.handler = async (event) => {
 
     try {
 
-        return {
-            statusCode: 200,
-            headers: util.getResponseHeaders(),
-            body: JSON.stringify('')
+        const noteId = decodeURIComponent(event.pathParameters.note_id);
+        const params = {
+            TableName: tableName,
+            IndexName: "note_id-index",
+            KeyConditionExpression: "note_id = :nId",
+            ExpressionAttributeValues: {
+                ":nId": noteId
+            },
+            Limit: 1
         };
+        const data = await dynamoDb.query(params).promise();
+
+        if (!_.isEmpty(data.Items)) {
+            return {
+                statusCode: 200,
+                headers: util.getResponseHeaders(),
+                body: JSON.stringify(data.Items[0])
+            };
+        } else {
+            return {
+                statusCode: 204,
+                headers: util.getResponseHeaders(),
+                body: JSON.stringify('')
+            };
+        }
 
     } catch (err) {
         console.log("Error", err);
