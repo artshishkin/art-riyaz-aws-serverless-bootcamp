4a3099da33cbc9528f81840581fdeb4038e5b3c0
Art <d.art.shishkin@gmail.com>
2022-02-04 17:34:32 +0200

    238. Generating the AWS Signature for Signing API Requests (#20)

--- sls/sls-notes-app/src/app/services/notes-api/notes-api.service.ts
+++ sls/sls-notes-app/src/app/services/notes-api/notes-api.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {AuthService} from "../auth/auth.service";
+import {Credentials, Request, RequestSigner} from "aws4";
 
 declare const API_ROOT: string;
 declare const STAGE: string;
@@ -8,28 +10,66 @@ declare const STAGE: string;
 @Injectable()
 export class NotesApiService {
     options;
-    constructor(private httpClient: HttpClient) {}
 
-    setOptions() {
-        this.options = {
+    constructor(private httpClient: HttpClient,
+                private authService: AuthService) {
+    }
+
+    setOptions(path = '/', method = '', body = '') {
+
+        const host = new URL(API_ROOT);
+        let args: Request = {
+            service: 'execute-api',
+            region: 'eu-north-1',
+            hostname: host.hostname,
+            path: path,
+            method: method,
+            body: body,
             headers: {
-                app_user_id: '123454321',
-                app_user_name: 'Art'
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
-        };
+        }
+
+        if (method == 'GET') {
+            delete args.body;
+        }
+        this.options = {};
+        try {
+            let savedCredsJson = this.authService.getCredentials();
+            if (savedCredsJson) {
+                let savedCreds = JSON.parse(savedCredsJson);
+                let creds: Credentials = {
+                    accessKeyId: savedCreds.Credentials.AccessKeyId,
+                    secretAccessKey: savedCreds.Credentials.SecretAccessKey,
+                    sessionToken: savedCreds.Credentials.SessionToken
+                };
+
+                let signer = new RequestSigner(args, creds);
+                let signed = signer.sign();
+                this.options.headers = signed.headers;
+                delete this.options.headers.Host;
+
+                this.options.headers.app_user_id = savedCreds.IdenityId;
+                this.options.headers.app_user_name = savedCreds.user_name;
+            }
+
+        } catch (err) {
+            //do nothing
+        }
     }
 
     addNote(item) {
         let path = STAGE + '/notes';
         let endpoint = API_ROOT + path;
-        
+
         let itemData;
         itemData = {
             content: item.content,
             cat: item.cat
         };
 
-        if(item.title != "") {
+        if (item.title != "") {
             itemData.title = item.title;
         }
 
@@ -43,7 +83,7 @@ export class NotesApiService {
     updateNote(item) {
         let path = STAGE + `/notes/n/${item.note_id}`;
         let endpoint = API_ROOT + path;
-        
+
         let itemData;
         itemData = {
             content: item.content,
@@ -70,10 +110,13 @@ export class NotesApiService {
         return this.httpClient.delete(endpoint, this.options);
     }
 
-    getNotes(start?): Observable<any> {
+    getNotes(start ?)
+        :
+        Observable<any> {
         let path = STAGE + '/notes?limit=8';
-        
-        if (start > 0) {
+
+        if (start > 0
+        ) {
             path += '&start=' + start;
         }
         let endpoint = API_ROOT + path;
